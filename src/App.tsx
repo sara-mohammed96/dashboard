9a0392ed533e280e17149af6d80abfaf66104fcd
Sarah Allateef <sara.korea96@gmail.com>
2024-03-11 20:50:01 +0300

    workflow setup for the routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import "@refinedev/antd/dist/reset.css";
 
 import { authProvider, dataProvider, liveProvider } from "./providers"
 
+import { Home, ForgotPassword, Login, Register } from './pages'
 import routerBindings, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
@@ -39,6 +40,10 @@ function App() {
               >
                 <Routes>
                   <Route index element={<WelcomePage />} />
+                  <Route index element={<Home />} />
+                  <Route path="/register"  element={<Register />} />
+                  <Route path="/login"  element={<Login />} />
+                  <Route path="/forgot-password"  element={<ForgotPassword />} />
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
