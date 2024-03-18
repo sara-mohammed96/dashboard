10aa1c52b681b16590b9c8e6b867095774d587e5
Sarah Allateef <sara.korea96@gmail.com>
2024-03-18 20:57:01 +0300

    working on the layout and the custome user setup

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,5 +1,21 @@
 import {Popover, Button } from 'antd'
+import CustomeAvatar from '../custome-avatar'
+import { useGetIdentity } from '@refinedev/core'
+
+import type { User } from '@/graphql/schema.types'
+import { Text  } from '../text'
 const CurrentUser =() => {
+    const { data: user } = useGetIdentity<User>()
+
+    const content = (
+        <div style={{ 
+            display:'flex',
+            flexDirection:'column',
+        }}>
+           <Text strong style={{ padding:'12px 20px'}}> {user?.name}
+            </Text> 
+        </div>
+    )
     return (
         <>
         <Popover
@@ -8,7 +24,12 @@ const CurrentUser =() => {
         overlayInnerStyle={{ padding: 0 }}
         overlayStyle={{ zIndex: 999 }}
         >
-            Test
+           <CustomeAvatar 
+              name={user?.name}
+              src={user?.avatarUrl}
+              size="default"
+              style={{ cursor: 'pointer' }}
+           />
         </Popover>
         </>
     )
